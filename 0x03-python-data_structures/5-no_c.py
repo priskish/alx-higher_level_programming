f9e515e6cf66ab965f7ff92cc2894fbeac7aed98
priskish <101426953+priskish@users.noreply.github.com>
2022-07-04 23:26:18 +0100

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_str = ""
-    for i in my_string:
-        if i is not 'c' and i is not 'C':
-            new_str += i
-    return (new_str)
-# return my_string.translate({ord(c): None for c in "cC"})
+    counts = my_string.count('c')
+    new_string = list(my_string)
+    while counts:
+        new_string.remove('c')
+        counts -= 1
+    counts = my_string.count('C')
+    while counts:
+        new_string.remove('C')
+        counts -= 1
+    new_string = ''.join(new_string)
+    return new_string
